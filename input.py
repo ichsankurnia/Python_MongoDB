69ec709983bae9f3183a5c1b4cfb8b7070b2e69f
ichsankurnia <ichsankurniawan51@gmail.com>
2019-09-04 19:25:17 +0700

    use pprint to show data

--- input.py
+++ input.py
@@ -1,12 +1,15 @@
 from pymongo import *
 import datetime
+from pprint import *
 
 # client = MongoClient('localhost', 27017)
 client = MongoClient('mongodb://localhost:27017/')
 
 db = client.testing_db # client.['testing_db']
 
-collection = db.collection1 # db.['collection1']
+# create collection
+collection1 = db.collection1
+# collection1 = db.['collection1']
 
 post = {
     "title": "Doc One",
@@ -16,7 +19,10 @@ post = {
     "date": datetime.datetime.now()
 }
 
-# posts = db.posts
+# insert doeccuments
 post_id = db.collection1.insert(post)
 
-print(post_id)
+show_doc = collection1.find_one()
+
+# print(collection1.find_one())
+pprint(show_doc)
